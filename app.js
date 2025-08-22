e8d9755a6470b515def01d34f91f209f77afe5e0
Samuel Gaitkoski <samuel.gaitkoski@gmail.com>
2025-08-22 12:00:09 -0300

    Remove old manual CLI code and keep only yargs version in app.js.

--- app.js
+++ app.js
@@ -2,20 +2,6 @@ const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
 const notes = require('./notes');
 
-// get command + arguments from CLI
-const command = process.argv[2];
-const input = process.argv.slice(3).join(" ");
-
-if (command === "add") {
-  notes.addNote(input);
-} else if (command === "list") {
-  notes.listNotes();
-} else if (command === "remove") {
-  notes.removeNote(input);
-} else {
-  console.log("Unknown command. Try: add, list, remove");
-}
-
 yargs(hideBin(process.argv))
   .command(
     "list",
